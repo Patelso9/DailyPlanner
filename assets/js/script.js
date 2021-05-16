d589cd8e0d3fd03fae9b9a9ac58a2c2fa75aed29
Princess <sonali.patel.pkg@gmail.com>
2021-05-15 20:59:49 -0400

    table works

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,41 +9,51 @@ $( document ).ready(function() {
 
 // display current date & time
     var time= moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    $('#display-time').text(time);
+    $('#time-display').text(time);
 
     var row = ""
     //loop to dispaly 9am-18pm
-      for (var i= 9 ; i<=20; i++){
-        // Creation of the row elements
+      for (var i= 9 ; i<=24; i++){
+       
+        // Create row elements
+
         row = $(`<div class="row">`)
-        col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`)
-        col2 = $(`<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`)
-        col3 = $(`<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`)
+        col1 = $(`<div class ="col hour">${displayAmorPm(i)}</div>`)
+        col2 = $(`<div class ="col-8 inputcontent">
+                    <input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput">
+                    </div>`)
+        col3 = $(`<div class ="col">
+                    <button data-id="${i}" id="savePlanner" class="btn btn-success btn-block">
+                    <i class="fas fa-save"></i> Save</button></div>`)
         row.append(col1)
         row.append(col2)
         row.append(col3)
         $("#planner").append(row)
         getlocalStorage(i)
       }
+
+    //   function to enable save button
      $("button.btn.btn-success").click(function(e){
      var id = $(this).data("id")
      var inputText = $(this).parent().siblings().find("input").val()
      localStorage.setItem(id,inputText)
      })
-    //  Convert Am to Pm
+    
+     //  Convert Am to Pm
      function displayAmorPm(hour){
-       var b=""
+       var timeOD=""
        if(hour<=12){
-         b= "AM"
+         timeOd= "AM"
        }else{
-         b="PM"
+         timeOd="PM"
        }
        hour = hour % 12
        hour = hour ? hour : 12
-       return hour + " " + b
+       return hour + " " + timeOd
      }
     });
-  
+
+    // setting up local storage to save text
      function getlocalStorage(hour){
        let inputval = localStorage.getItem(hour)
        if(true){
@@ -52,20 +62,21 @@ $( document ).ready(function() {
         console.log(text)
        }
      }
-     //Apdate color
-     function updateColor(){
-       var hour = new Date().getHours();
-       for (var i= 9 ; i<=18; i++){
-         console.log(hour,i)
-         if(hour==i ) {
-          $(`#inputText${i}`).css("background","red")
-         }else  if(hour<i ){
+
+    // //  update color- does not work
+    //  function updateColor(){
+    //    var hour = new Date().getHours();
+    //    for (var i= 9 ; i<=18; i++){
+    //      console.log(hour,i)
+    //      if(hour==i ) {
+    //       $(`#inputText${i}`).css("background","red")
+    //      }else  if(hour<i ){
           
-           $(`#inputText${i}`).css("background","lightblue")
+    //        $(`#inputText${i}`).css("background","lightblue")
   
-         }
-       }
-     }  
+    //      }
+    //    }
+    //  }  
 
 setInterval(function(){
 
